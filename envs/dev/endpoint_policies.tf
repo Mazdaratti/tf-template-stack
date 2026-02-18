e617e8e78f702d9d6c47bbe205306cab8d773b98
Mazdaratti <vasya.mazdaratti@googlemail.com>
2026-02-18 21:11:23 +0100

    chore(env/dev): add active endpoint_policies file

---
+++ envs/dev/endpoint_policies.tf
@@ -0,0 +1,17 @@
+############################################
+# Endpoint policies (ACTIVE)
+############################################
+#
+# This file is loaded by Terraform (because it ends with `.tf`).
+#
+# Add ACTIVE endpoint policy documents here and wire them in `main.tf`
+# using `endpoint_policy_json` for:
+# - module "vpc_gateway_endpoints"
+# - module "vpc_interface_endpoints"
+#
+# If you do not define or attach policies, AWS default endpoint policies apply.
+#
+# Examples and templates live in:
+# - endpoint_policies.tf.example
+#
+############################################
