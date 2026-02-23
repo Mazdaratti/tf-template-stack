6903ecaa273bca3ec4fa2dbb1f0ee6ff60b6d49c
Mazdaratti <vasya.mazdaratti@googlemail.com>
2026-02-23 20:24:07 +0100

    docs(logging_baseline): add basic example outputs.tf

---
+++ modules/logging_baseline/examples/basic_usage/outputs.tf
@@ -0,0 +1,15 @@
+############################################
+# Outputs
+#
+# Outputs make it easy to see what was created after `terraform apply`.
+############################################
+
+output "log_group_names" {
+  description = "Created log group names (keyed by log_groups keys)."
+  value       = module.logging_baseline.log_group_names
+}
+
+output "log_group_arns" {
+  description = "Created log group ARNs (keyed by log_groups keys)."
+  value       = module.logging_baseline.log_group_arns
+}
