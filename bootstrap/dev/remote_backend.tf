d36418404d7ec400c6dc6010b553858a8cf05ca1
Mazdaratti <vasya.mazdaratti@googlemail.com>
2026-03-17 15:26:10 +0100

    fix(bootstrap): remove state bucket versioning from dev backend

--- bootstrap/dev/remote_backend.tf
+++ bootstrap/dev/remote_backend.tf
@@ -24,14 +24,6 @@ resource "aws_s3_bucket" "terraform_state" {
   })
 }
 
-resource "aws_s3_bucket_versioning" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
 resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
   bucket = aws_s3_bucket.terraform_state.id
 
